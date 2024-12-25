234f29f6639e9cbc4703a9cc38f22c25c958bbef
Jay Steinle <jay.steinle62@gmail.com>
2024-12-25 12:43:07 -0600

    tryCatch

---
+++ exceptions/exercises/tempCodeRunnerFile.js
@@ -0,0 +1,22 @@
+ntLabs2 = [
+//   {
+//       student: 'Blake',
+//       myCode: function (num) {
+//         return Math.pow(num, num);
+//       }
+//   },
+//   {
+//       student: 'Jessica',
+//       runLab: function (num) {
+//         return Math.pow(num, num);
+//       }
+//   },
+//   {
+//       student: 'Mya',
+//       runLab: function (num) {
+//         return num * num;
+//       }
+//   }
+// ];
+
+// g
